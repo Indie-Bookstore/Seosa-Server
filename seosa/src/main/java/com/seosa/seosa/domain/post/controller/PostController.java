354903942e79ebb3a7818231652c532967b2ef09
oohyj <hyj0660@gmail.com>
2025-03-06 23:11:16 +0900

    feat :스웨거 관련 코드 추가

--- seosa/src/main/java/com/seosa/seosa/domain/post/controller/PostController.java
+++ seosa/src/main/java/com/seosa/seosa/domain/post/controller/PostController.java
@@ -4,6 +4,8 @@ import com.seosa.seosa.domain.post.dto.Request.PostReqDto;
 import com.seosa.seosa.domain.post.dto.Response.PostResDto;
 import com.seosa.seosa.domain.post.service.PostService;
 import com.seosa.seosa.domain.user.entity.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +15,14 @@ import com.seosa.seosa.global.annotation.AuthUser;
 @RestController
 @RequestMapping("/post")
 @RequiredArgsConstructor
+@Tag(name = "Post API", description = "글 관련 API")
 public class PostController {
 
     private final PostService postService;
 
+    /*글 작성 api */
     @PostMapping
+    @Operation(summary = "글 작성", description = "ADMIN 사용자가 새로운 글을 작성합니다.글 , 서점 , 서점 제품이 포함되어야 함")
     public ResponseEntity<PostResDto> registerPost(@AuthUser User user , @RequestBody PostReqDto postReqDto){
         PostResDto postResDto = postService.registerPost(user, postReqDto);
         return ResponseEntity.status(HttpStatus.CREATED)
