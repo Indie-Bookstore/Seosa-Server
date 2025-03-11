ca71cf92c4085df4bfb3f0e80eb961ea410511ea
unul09 <morning20404@gmail.com>
2025-03-12 07:10:31 +0900

    refactor: auth_whitelist sync with JWTFilter, change user api response to json

--- seosa/src/main/java/com/seosa/seosa/domain/user/controller/UserController.java
+++ seosa/src/main/java/com/seosa/seosa/domain/user/controller/UserController.java
@@ -15,6 +15,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 
 @RequestMapping("/user")
@@ -35,64 +37,63 @@ public class UserController {
 
     @PatchMapping("/profile")
     @Operation(summary = "회원 프로필 수정", description = "회원 닉네임 또는 프로필 사진 정보를 수정합니다.")
-    public ResponseEntity<String> updateUserProfile(
+    public ResponseEntity<Map<String, String>> updateUserProfile(
             @AuthUser User user,
             @RequestBody @Valid UpdateProfileRequestDTO request) {
         userService.updateUserProfile(user, request);
-        return ResponseEntity.ok("프로필 수정이 완료되었습니다.");
+        return ResponseEntity.ok(Map.of("message", "프로필 수정이 완료되었습니다."));
     }
 
     @PatchMapping("/password")
     @Operation(summary = "회원 비밀번호 수정", description = "회원 비밀번호 정보를 수정합니다.")
-    public ResponseEntity<String> updateUserPassword(
+    public ResponseEntity<Map<String, String>> updateUserPassword(
             @AuthUser User user,
             @RequestBody @Valid UpdatePasswordRequestDTO request) {
         userService.updateUserPassword(user, request);
-        return ResponseEntity.ok("비밀번호 변경이 완료되었습니다.");
+        return ResponseEntity.ok(Map.of("message", "비밀번호 변경이 완료되었습니다."));
     }
 
     @DeleteMapping
     @Operation(summary = "회원 탈퇴", description = "회원을 탈퇴합니다.")
-    public ResponseEntity<String> deleteUser(@AuthUser User user) {
+    public ResponseEntity<Map<String, String>> deleteUser(@AuthUser User user) {
         userService.deleteUser(user);
-        return ResponseEntity.ok("회원 탈퇴가 완료되었습니다.");
+        return ResponseEntity.ok(Map.of("message", "회원 탈퇴가 완료되었습니다."));
     }
 
 
     @GetMapping("/checkNickname")
     @Operation(summary = "닉네임 중복확인", description = "닉네임 중복 여부를 확인하고 유효성을 검증합니다.")
-    public ResponseEntity<String> checkNickname(@RequestParam ("nickname") String nickname) {
-
+    public ResponseEntity<Map<String, String>> checkNickname(@RequestParam ("nickname") String nickname) {
         checkService.checkNickname(nickname);
-        return ResponseEntity.ok("사용 가능한 닉네임입니다.");
+        return ResponseEntity.ok(Map.of("message", "사용 가능한 닉네임입니다."));
     }
 
 
     @GetMapping("/checkEmail")
     @Operation(summary = "이메일 중복확인", description = "이메일 중복 여부를 확인하고 유효성을 검증합니다.")
-    public ResponseEntity<String> checkEmail(@RequestParam("email") String email) {
+    public ResponseEntity<Map<String, String>> checkEmail(@RequestParam("email") String email) {
 
         checkService.checkEmail(email);
-        return ResponseEntity.ok("사용 가능한 이메일입니다.");
+        return ResponseEntity.ok(Map.of("message", "사용 가능한 이메일입니다."));
     }
 
     @GetMapping("/sendVerificationCode")
     @Operation(summary = "인증번호 전송", description = "회원 이메일로 인증번호를 전송합니다.")
-    public ResponseEntity<String> sendVerificationCode(@AuthUser User user,
+    public ResponseEntity<Map<String, String>> sendVerificationCode(@AuthUser User user,
                                                        @RequestParam("email")
                                                        String email) {
 
         verificationService.sendVerificationCode(user, email);
-        return ResponseEntity.ok("이메일로 인증번호를 전송했습니다.");
+        return ResponseEntity.ok(Map.of("message", "이메일로 인증번호를 전송했습니다."));
     }
 
     @GetMapping("/checkVerificationCode")
     @Operation(summary = "본인인증", description = "입력한 인증번호가 전송된 인증코드와 일치하는지 확인합니다.")
-    public ResponseEntity<String> checkVerificationCode(@AuthUser User user,
+    public ResponseEntity<Map<String, String>> checkVerificationCode(@AuthUser User user,
                                                         @RequestParam("verificationCode")
                                                         String verificationCode) {
 
         verificationService.checkVerificationCode(user, verificationCode);
-        return ResponseEntity.ok("인증번호가 확인되었습니다.");
+        return ResponseEntity.ok(Map.of("message", "인증번호가 확인되었습니다."));
     }
 }
