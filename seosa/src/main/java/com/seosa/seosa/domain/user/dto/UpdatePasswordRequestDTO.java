aadb1aaa4d46bcb87332223aac0b65bd5836e1de
wonwoo Kim <39664609+unul09@users.noreply.github.com>
2025-03-13 21:44:29 +0900

    cors setting
    * login error loging
    
    * error solved. rollback to before code state
    
    * changed cors setting to allow domain
    
    * updatepassword pattern fix
    
    * changed cors allow lists to array
    
    * added cors patch method

--- seosa/src/main/java/com/seosa/seosa/domain/user/dto/UpdatePasswordRequestDTO.java
+++ seosa/src/main/java/com/seosa/seosa/domain/user/dto/UpdatePasswordRequestDTO.java
@@ -10,12 +10,12 @@ import lombok.Setter;
 public class UpdatePasswordRequestDTO {
 
     @NotBlank(message = "새 비밀번호를 입력해주세요.")
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d!@#$%^&*]{8,}$",
             message = "비밀번호는 최소 8자 이상이며, 최소 1개 이상의 영문자와 숫자를 포함해야 합니다.")
     private String newPassword1;
 
     @NotBlank(message = "새 비밀번호를 입력해주세요.")
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d!@#$%^&*]{8,}$",
             message = "비밀번호는 최소 8자 이상이며, 최소 1개 이상의 영문자와 숫자를 포함해야 합니다.")
     private String newPassword2;
 }
